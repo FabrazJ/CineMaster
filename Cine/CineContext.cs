dfb829e3c559ddbef6085463f2ff4383a74a08fd
FabrazJVargas <m.vargas.or10@gmail.com>
2024-05-01 22:10:58 -0500

    Add project files.

---
+++ Cine/CineContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Cine
+{
+    public class CineContext : DbContext
+    {
+      public CineContext(DbContextOptions<CineContext> options)
+        :base(options)
+        {
+        
+        }
+
+        public object Bookings { get; set; }
+        public object Billboards { get; set; }
+        public object Movies { get; set; }
+    }
+}
---
+++ Cine/EntityDb.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cine
+{
+    public class EntityDb : DbContext
+    {
+        public DbSet<BillboardEntity> Billboards { get; set; }
+        public DbSet<BookingEntity> Bookings { get; set; }
+        public DbSet<CustomerEntity> Customers { get; set; }
+        public DbSet<MovieEntity> Movies { get; set; }
+        public DbSet<RoomEntity> Rooms { get; set; }
+        public DbSet<SeatEntity> Seats { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("your_connection_string_here");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BookingEntity>()
+                .HasOne(b => b.Customer)
+                .WithMany()
+                .HasForeignKey(b => b.CustomerId);
+
+            modelBuilder.Entity<BookingEntity>()
+                .HasOne(b => b.Seat)
+                .WithMany()
+                .HasForeignKey(b => b.SeatId);
+
+            modelBuilder.Entity<BookingEntity>()
+                .HasOne(b => b.Billboard)
+                .WithMany()
+                .HasForeignKey(b => b.BillboardId);
+
+
+            modelBuilder.Entity<BillboardEntity>()
+                .HasOne(b => b.Movie)
+                .WithMany()
+                .HasForeignKey(b => b.MovieId);
+
+            modelBuilder.Entity<BillboardEntity>()
+                .HasOne(b => b.Room)
+                .WithMany()
+                .HasForeignKey(b => b.RoomId);
+
+            modelBuilder.Entity<SeatEntity>()
+                .HasOne(s => s.Room)
+                .WithMany()
+                .HasForeignKey(s => s.RoomId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
